c4ee902158065b4a197fa0d40f77cb05cabe4425
Vlad <vladvesinin676@gmail.com>
2025-04-28 20:08:05 +0500

    created db

---
+++ HamsterCombat/Database/Database.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HamsterCombat.Database
+{
+    public class Database
+    {
+        private string db_path = "./db.sqlite";
+        
+        public void UpdateBalance(int balance)
+        {
+            string sqlExpression = $"UPDATE player_info SET balance={balance} WHERE id=0";
+            using (var connection = new SqliteConnection($"Data Source={db_path}"))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+
+                int number = command.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- HamsterCombat/ViewModels/Pages/ClickerViewModel.cs
+++ HamsterCombat/ViewModels/Pages/ClickerViewModel.cs
@@ -1,11 +1,14 @@
-﻿using ReactiveUI;
+﻿using HamsterCombat.Models;
+using ReactiveUI;
+using System.Data.Common;
 using System.Reactive;
 
 namespace HamsterCombat.ViewModels.Pages;
 
 public class ClickerViewModel : PageBaseModel
 {
-    public ReactiveCommand<Unit, Unit> ClickCommand { get; }
+    private readonly Player _player = new Player();
+
     private int _balance;
     public int Balance
     {
@@ -13,14 +16,21 @@ public class ClickerViewModel : PageBaseModel
         set => this.RaiseAndSetIfChanged(ref _balance, value);
     }
 
+    public ReactiveCommand<Unit, Unit> ClickCommand { get; }
+
     public ClickerViewModel()
     {
         Title = "Кликер";
+
+        // Инициализируем баланс из Player
+        _balance = _player.Balance_;
+
         ClickCommand = ReactiveCommand.Create(Add);
     }
 
     private void Add()
     {
         Balance++;
+        _player.Balance_ = Balance;
     }
 }
